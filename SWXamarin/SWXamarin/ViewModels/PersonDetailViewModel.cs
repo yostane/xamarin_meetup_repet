c05ff050331f03b2e08ca42adedcddb9877eb9b2
yostane <yostane@yahoo.fr>
2018-10-08 21:32:04 +0200

    Ajout de la page PersonDetail + view model

---
+++ SWXamarin/SWXamarin/ViewModels/PersonDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xamarin.Forms;
+using RandomListXamarin.ViewModels;
+using SharpTrooper.Entities;
+
+namespace SWXamarin.ViewModels
+{
+    public class PersonDetailViewModel : BaseViewModel
+    {
+        private People _person;
+        public People Person
+        {
+            get => _person;
+            set => SetProperty(ref _person, value);
+        }
+    }
+}
+
---
+++ SWXamarin/SWXamarin/Views/PersonDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using SWXamarin.ViewModels;
+
+namespace SWXamarin.Views
+{
+    public partial class PersonDetailPage : ContentPage
+    {
+        public PersonDetailPage(PersonDetailViewModel personDetailViewModel)
+        {
+            InitializeComponent();
+            BindingContext = personDetailViewModel;
+        }
+    }
+}
